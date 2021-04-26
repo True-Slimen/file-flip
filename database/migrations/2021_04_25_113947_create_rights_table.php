fbc0c8b621e64a42a07d27984f985633bc3b8366
Slimen <metatidj.slimen@gmail.com>
2021-04-26 19:53:05 +0200

    model et migrations ok

---
+++ database/migrations/2021_04_25_113947_create_rights_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rights', function (Blueprint $table) {
+            $table->id();
+            $table->integer('userid')->nullable();
+            $table->integer('groupeid')->nullable();
+            $table->integer('fileid');
+            $table->string('name');
+            $table->integer('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rights');
+    }
+}
