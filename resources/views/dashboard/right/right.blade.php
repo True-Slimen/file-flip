248779974407bf8a34cebed85f037e646b20aba8
Slimen <metatidj.slimen@gmail.com>
2021-05-12 00:08:51 +0200

    validation de la cré de droits pour file et folder

--- resources/views/dashboard/right/right.blade.php
+++ resources/views/dashboard/right/right.blade.php
@@ -154,8 +154,19 @@
                     </div>
                 </form>
         </div>
+        
     </div>
-
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+    @elseif ($message = Session::get('error'))
+    <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
 </section>
 
 
